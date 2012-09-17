c6a99558cef0e2a876e4fbb31e74d8847273f998
techxplorer <corey@servalproject.org>
2012-09-17 12:42:00 +0930

    add license information to required files

--- src/org/magdaaproject/mem/LauncherActivity.java
+++ src/org/magdaaproject/mem/LauncherActivity.java
@@ -1,17 +1,50 @@
+/*
+ * Copyright (C) 2012 The MaGDAA Project
+ *
+ * This file is part of the MaGDAA MEM Software
+ *
+ * MaGDAA MEM Software is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package org.magdaaproject.mem;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
+/**
+ * 
+ * The launcher activity for the MEM software, this is the default activity displayed
+ * to the user when the application starts
+ *
+ */
 public class LauncherActivity extends Activity {
 
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
     }
 
+    /*
+     * (non-Javadoc)
+     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_launcher, menu);
