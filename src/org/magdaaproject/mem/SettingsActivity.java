5036b72fc0b083eb3a1cd19a1eb45964ee2f31e8
techxplorer <corey@servalproject.org>
2012-10-04 11:27:03 +0930

    refine the preferences activity

--- src/org/magdaaproject/mem/SettingsActivity.java
+++ src/org/magdaaproject/mem/SettingsActivity.java
@@ -19,10 +19,20 @@
  */
 package org.magdaaproject.mem;
 
+import org.magdaaproject.utils.GeoCoordUtils;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * the settings activity allows the user to change the various settings / preferences
@@ -32,9 +42,17 @@ import android.preference.PreferenceActivity;
 public class SettingsActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	
 	/*
-	 * private class level variables
+	 * private class level constants
 	 */
-	private static final String sLocationInfoSource = "preferences_collection_location_type";
+	private static final String sTag = "SettingsActivity";
+	
+	private static final String sLocationInfoGps = "preferences_collection_location_gps";
+	private static final String sLocationInfoManual = "preferences_collection_location_manual";
+	
+	private static final String sManualLatitude = "preferences_collection_location_manual_lat";
+	private static final String sManualLongitude = "preferences_collection_location_manual_lng";
+	
+	private static final int sMissingCoordsDialog = 0;
 	
 	/*
 	 * (non-Javadoc)
@@ -45,23 +63,160 @@ public class SettingsActivity extends PreferenceActivity implements OnSharedPref
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        
+        // listen for changes to the preferences
+        SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        mPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
 	/*
 	 * (non-Javadoc)
 	 * @see android.content.SharedPreferences.OnSharedPreferenceChangeListener#onSharedPreferenceChanged(android.content.SharedPreferences, java.lang.String)
 	 */
+	@SuppressWarnings("deprecation")
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
 		
-		// find out which preference was just changed
 		// TODO use this to update the summary of the list items
-		if(key.equals(sLocationInfoSource)) {
-			String mValue = sharedPreferences.getString(key, null);
+		
+		//debug code
+		Log.v(sTag, "preference key: '" + key + "'");
+		
+		/*
+		 *  ensure the appropriate coordinate checkbox is ticked
+		 */
+		if(key.equals(sLocationInfoGps)) {
 			
-			if(mValue.equals("manual")) {
-				// show dialogs for coordinates
+			Boolean mValue = sharedPreferences.getBoolean(key, false);
+			
+			// uncheck the manual location check box
+			SharedPreferences.Editor mEditor = sharedPreferences.edit();
+			mEditor.putBoolean(sLocationInfoManual, !mValue);
+			mEditor.apply();
+			
+			CheckBoxPreference mPreference = (CheckBoxPreference) findPreference(sLocationInfoManual);
+			mPreference.setChecked(!mValue);	
+		}
+		
+		if(key.equals(sLocationInfoManual)) {
+			
+			Boolean mValue = sharedPreferences.getBoolean(key, false);
+			
+			// uncheck the gps location check box
+			SharedPreferences.Editor mEditor = sharedPreferences.edit();
+			mEditor.putBoolean(sLocationInfoGps, !mValue);
+			mEditor.apply();
+			
+			CheckBoxPreference mPreference = (CheckBoxPreference) findPreference(sLocationInfoGps);
+			mPreference.setChecked(!mValue);
+		}
+		
+		/*
+		 * ensure the manual coordinates are valid
+		 */
+		if(key.equals(sManualLatitude)) {
+			
+			String mValue = sharedPreferences.getString(sManualLatitude, null);
+			
+			if(mValue != null && mValue.equals("") == false) {
+				if(GeoCoordUtils.isValidLatitude(Float.parseFloat(mValue)) == false) {
+					
+					// remove the erroneous data
+					SharedPreferences.Editor mEditor = sharedPreferences.edit();
+					mEditor.putString(sManualLatitude, null);
+					mEditor.apply();
+					
+					EditTextPreference mPreference = (EditTextPreference) findPreference(sManualLatitude);
+					mPreference.setText("");
+					
+					// show a toast
+					Toast.makeText(getApplicationContext(), R.string.preferences_collection_location_manual_invalid, Toast.LENGTH_LONG).show();
+					
+				}
 			}
+		}
+		
+		if(key.equals(sManualLongitude)) {
+			
+			String mValue = sharedPreferences.getString(sManualLongitude, null);
+			
+			if(mValue != null) {
+				if(GeoCoordUtils.isValidLongitude(Float.parseFloat(mValue)) == false) {
+					
+					// remove the erroneous data
+					SharedPreferences.Editor mEditor = sharedPreferences.edit();
+					mEditor.putString(sManualLongitude, null);
+					mEditor.apply();
+					
+					EditTextPreference mPreference = (EditTextPreference) findPreference(sManualLongitude);
+					mPreference.setText("");
+					
+					// show a toast
+					Toast.makeText(getApplicationContext(), R.string.preferences_collection_location_manual_invalid, Toast.LENGTH_LONG).show();
+					
+				}
+			}
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onBackPressed()
+	 */
+	@Override
+	@SuppressWarnings("deprecation")
+	public void onBackPressed(){
+		/*
+		 * ensure that if manual location is enabled that coordinates have been specified
+		 */
+		
+		SharedPreferences mPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+		
+		boolean isManualLocation = mPreferences.getBoolean(sLocationInfoManual, false);
+		
+		if(isManualLocation) {
+			// manual location is enabled
+			
+			String mLatitude = mPreferences.getString(sManualLatitude, null);
+			String mLongitude = mPreferences.getString(sManualLongitude, null);
+			
+			if(mLatitude == null || mLongitude == null) {
+				// show an error dialog
+				showDialog(sMissingCoordsDialog);
+			} else {
+				// finish as normal
+				finish();
+			}
+		} else {
+			// finish as normal
+			finish();
+		}
+	}
+	
+	/*
+	 * callback method used to construct the required dialog
+	 * (non-Javadoc)
+	 * @see android.app.Activity#onCreateDialog(int)
+	 */
+	@SuppressWarnings("deprecation")
+	@Override
+	protected Dialog onCreateDialog(int id) {
+
+		AlertDialog.Builder mBuilder = new AlertDialog.Builder(this);
+		
+		// determine which dialog to show
+		switch(id) {
+		case sMissingCoordsDialog:
+			mBuilder.setMessage(R.string.preferences_dialog_invalid_manual_location_coords)
+			.setCancelable(false)
+			.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id) {
+					dialog.cancel();
+				}
+			});
+			return mBuilder.create();
+		default:
+			return super.onCreateDialog(id);
 		}
 	}
 }
